d1708d200b0d6dd1260876146b909fbe381f6e9e
David Lumley <david@davidlumley.xyz>
2024-04-09 17:14:48 -0700

    feat(nextjs): Pull ephemeralPublicKey when rendering ClerkProvider on the server

--- packages/nextjs/src/app-router/server/ClerkProvider.tsx
+++ packages/nextjs/src/app-router/server/ClerkProvider.tsx
@@ -1,4 +1,6 @@
+import { constants } from '@clerk/backend/internal';
 import type { InitialState, Without } from '@clerk/types';
+import { cookies } from 'next/headers';
 import React from 'react';
 
 import type { NextClerkProviderProps } from '../../types';
@@ -6,13 +8,19 @@ import { mergeNextClerkPropsWithEnv } from '../../utils/mergeNextClerkPropsWithE
 import { ClientClerkProvider } from '../client/ClerkProvider';
 import { initialState } from './auth';
 
+const fetchEphemeralCookie = () => {
+  const { get } = cookies();
+  return get(constants.Cookies.PublishableKey)?.value;
+};
+
 export function ClerkProvider(props: Without<NextClerkProviderProps, '__unstable_invokeMiddlewareOnAuthStateChange'>) {
   const { children, ...rest } = props;
   const state = initialState()?.__clerk_ssr_state as InitialState;
+  const publishableKey = rest.publishableKey || fetchEphemeralCookie();
 
   return (
     <ClientClerkProvider
-      {...mergeNextClerkPropsWithEnv(rest)}
+      {...mergeNextClerkPropsWithEnv({ ...rest, publishableKey })}
       initialState={state}
     >
       {children}
