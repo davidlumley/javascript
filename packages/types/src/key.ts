70c54055cd5aaae81d94979cb02a88594381a8ba
David Lumley <david@davidlumley.xyz>
2024-05-09 22:12:38 -0700

    feat(repo): Add fetchEphemeralKeys

--- packages/types/src/key.ts
+++ packages/types/src/key.ts
@@ -4,3 +4,9 @@ export type PublishableKey = {
   frontendApi: string;
   instanceType: InstanceType;
 };
+
+export type EphemeralKeys = {
+  publishableKey: string;
+  secretKey: string;
+  expiresAt?: number;
+};
