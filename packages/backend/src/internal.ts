befcbd481604c07d1cc109e2c2498f4f50c11d04
David Lumley <david@davidlumley.xyz>
2024-07-18 01:46:13 -0700

    feat(nextjs): add ephemeral mode
    Co-authored-by: Ayrton <hello@ayrton.be>

--- packages/backend/src/internal.ts
+++ packages/backend/src/internal.ts
@@ -2,25 +2,26 @@ export { constants } from './constants';
 export { createRedirect } from './createRedirect';
 export type { RedirectFun } from './createRedirect';
 
-export type { CreateAuthenticateRequestOptions } from './tokens/factory';
 export { createAuthenticateRequest } from './tokens/factory';
+export type { CreateAuthenticateRequestOptions } from './tokens/factory';
 
 export { debugRequestState } from './tokens/request';
 
 export type { AuthenticateRequestOptions } from './tokens/types';
 
+export { makeAuthObjectSerializable, signedInAuthObject, signedOutAuthObject } from './tokens/authObjects';
 export type {
-  SignedInAuthObjectOptions,
-  SignedInAuthObject,
-  SignedOutAuthObject,
   AuthObject,
+  SignedInAuthObject,
+  SignedInAuthObjectOptions,
+  SignedOutAuthObject,
 } from './tokens/authObjects';
-export { makeAuthObjectSerializable, signedOutAuthObject, signedInAuthObject } from './tokens/authObjects';
 
 export { AuthStatus } from './tokens/authStatus';
 export type { RequestState, SignedInState, SignedOutState } from './tokens/authStatus';
 
 export { decorateObjectWithResources, stripPrivateDataFromObject } from './util/decorateObjectWithResources';
+export { fetchEphemeralAccount } from './util/fetchEphemeralAccount';
 
 export { createClerkRequest } from './tokens/clerkRequest';
 export type { ClerkRequest } from './tokens/clerkRequest';
